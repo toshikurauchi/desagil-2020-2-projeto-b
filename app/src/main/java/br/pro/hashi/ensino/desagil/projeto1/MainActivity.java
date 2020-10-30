aceda09b4de127d7d15d33ae3abb967308e57af8
caioesr <caioesr@al.insper.edu.br>
2020-10-30 19:30:38 -0300

    prototipo main page feito

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -1,7 +1,7 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
